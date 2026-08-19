04f0da3d6c2508e86f77047c5b8a0be18db5fc55
AGF Coleta <agfcoleta@projeto.local>
2026-08-19 14:39:06 -0300

    fix: nome do projeto alterado de PAEBM - SAG para PAEBM

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Funciona offline após primeiro acesso
    ============================================ */
 
-const CACHE_NAME = 'agf-coleta-v49';
+const CACHE_NAME = 'agf-coleta-v50';
 const urlsToCache = [
     './',
     './index.html',
