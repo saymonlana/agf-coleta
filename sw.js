e36d1232e5e45508edca508116c809532c07ce0d
AGF Coleta <agfcoleta@projeto.local>
2026-08-19 14:22:44 -0300

    fix: versao cache v47 para forcar atualizacao mobile

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    Funciona offline após primeiro acesso
    ============================================ */
 
-const CACHE_NAME = 'agf-coleta-v46';
+const CACHE_NAME = 'agf-coleta-v47';
 const urlsToCache = [
     './',
     './index.html',
